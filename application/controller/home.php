eb75212aab2f0d375246ed2e15b98ed9bff98bb1
Alvin Lu <alvinlu@mail.sfsu.edu>
2016-11-14 23:41:07 -0800

    map_properties

--- application/controller/home.php
+++ application/controller/home.php
@@ -11,6 +11,15 @@ class Home extends Controller
         require APP . 'view/home/index.html';
         require APP . 'view/_templates/footer.php';
     }
+    
+    public function map_properties()
+    {   
+
+        // load views
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/home/map_properties.html';
+        require APP . 'view/_templates/footer.php';
+    }
 
 }
 
