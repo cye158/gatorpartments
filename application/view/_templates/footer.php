1f276030dd3150e2cddfba0bc0b90b9607155631
Alvin Lu <alvinlu@mail.sfsu.edu>
2016-11-14 23:35:04 -0800

    headers&footers

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -1,4 +1,5 @@
 
+   /*
     <!-- Jquery -->
     <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <!-- Bootstrap's Javascript file -->
@@ -10,6 +11,35 @@
     <script src="js/global.js"></script>
     <script src="js/.js"></script>
     <script src="js/application.js"></script>
+    **/
 
+    <!-- JQuery Minified -->
+    <script type="text/javascript" src="<?php echo URL;?>js/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="<?php echo URL;?>js/response.min.js"></script>
+
+
+    <!-- Important Owl stylesheet -->
+    <link rel="stylesheet" href="<?php echo URL;?>js/owl-carousel/owl.carousel.css">
+    <!-- Default Theme -->
+    <link rel="stylesheet" href="<?php echo URL;?>js/owl-carousel/owl.theme.css">
+    <!-- Include js plugin -->
+    <script src="<?php echo URL;?>js/owl-carousel/owl.carousel.js"></script>
+
+    <script type="text/javascript" src="<?php echo URL;?>js/jquery.aw-showcase/jquery.aw-showcase.js"></script>
+    <link rel="stylesheet" href="<?php echo URL;?>js/jquery.aw-showcase/css/style.css" />
+
+    <script src="<?php echo URL;?>bootstrap/js/bootstrap.min.js"></script>
+
+
+    <link rel="stylesheet" href="<?php echo URL;?>js/badger/badger.min.css">
+    <script type="text/javascript" src="<?php echo URL;?>js/badger/badger.min.js"></script>
+
+    <link rel="stylesheet" href="<?php echo URL;?>js/sticky/stick.min.css">
+    <script type="text/javascript" src="<?php echo URL;?>js/sticky/sticky.min.js"></script>
+
+    <script type="text/javascript" src="<?php echo URL;?>js/portamento-min.js"></script>
+    <script type="text/javascript" src="<?php echo URL;?>js/global.js"></script>
+
+    
 </body>
 </html>
