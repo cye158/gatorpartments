2bce1f6bc764279e768a043d0e0ea03447892ba4
mbain <mbain@mail.sfsu.edu>
2016-11-17 22:34:10 -0800

    Continuing to troubleshoot header/footer issue.

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -1,3 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-  
+<head>
+  <meta charset="utf-8">
+  <title>gatorpartments</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+
+
+  <!-- CSS Bootstrap -->
+
+
+  <link href="<?php echo URL; ?>boostrap/css/boostrap.min.css" rel="stylesheet">
+
+  <link id="switch_style" href="<?php echo URL; ?>css/real_estate" rel="stylesheet">
+  <link href="<?php echo URL; ?>css/theme.css" rel="stylesheet">
+
+  <!-- <link rel="stylesheet" href="css/style.css"> -->
+
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!--[if lt IE 9]>
+  <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+
+</head>
--- application/view/home/index.php
+++ application/view/home/index.php
@@ -1,38 +1,3 @@
-<head>
-	<meta charset="utf-8">
-	<title>gatorpartments</title>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-	<meta name="description" content="">
-	<meta name="author" content="">
-
-
-
-	<!-- CSS Bootstrap -->
-
-
-	<link href="<?php echo URL; ?>boostrap/css/boostrap.min.css" rel="stylesheet">
-
-	<link id="switch_style" href="<?php echo URL; ?>css/real_estate" rel="stylesheet">
-	<link href="<?php echo URL; ?>css/theme.css" rel="stylesheet">
-
-	<!-- <link rel="stylesheet" href="css/style.css"> -->
-
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-
-	<!--[if lt IE 9]>
-	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-
-
-</head>
 <body>
 	<div class="container">
 	<?php require APP . 'view/_templates/navbar.php'; ?>
@@ -448,26 +413,5 @@
 </footer>
 
 </div> <!-- /container -->
-
-<div id="theme_switcher">
-	<div class="btn-group">
-		<a class="btn btn-success dropdown-toggle" data-toggle="dropdown" href="#">Switch theme <span class="caret"></span></a>
-		<ul class="dropdown-menu">
-            <li><a href="#" data-theme="real_estate">Real Estate</a></li>
-			<li><a href="#" data-theme="cerulean">Cerulean</a></li>
-			<li><a href="#" data-theme="lumen">Lumen</a></li>
-            <li><a href="#" data-theme="slate">Slate</a></li>
-			<li><a href="#" data-theme="united">United</a></li>
-			<li><a href="#" data-theme="cyborg">Cyborg</a></li>
-            <li><a href="#" data-theme="spacelab">Spacelab</a></li>
-			<li><a href="#" data-theme="journal">Journal</a></li>
-			<li><a href="#" data-theme="simplex">Simplex</a></li>
-            <li><a href="#" data-theme="superhero">Superhero</a></li>
-            <li><a href="#" data-theme="amelia">Amelia</a></li>
-			<li><a href="#" data-theme="cosmo">Cosmo</a></li>
-			<li><a href="#" data-theme="flatly">Flatly</a></li>
-		</ul>
-	</div>
-</div>
 
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
