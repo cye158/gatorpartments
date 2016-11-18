84d3e1f9f70645ece4be214c84a7bc4455ecd473
Xuan Zhang <xzhang8@mail.sfsu.edu>
2016-11-18 04:22:35 +0000

    1.add inputUserName method in home.php (controller), just for my version working, when merging, please use $_SESSION variable instead. 2.add showMessage method in message.php (controller).

--- application/controller/messages.php
+++ application/controller/messages.php
@@ -3,15 +3,32 @@
 class Messages extends Controller
 {
     public function addMessage()
-    {if ($_SESSION['loggedIn'])
-	if (isset($_POST["submit_add_message"])) {
-	    $userId = $this->messageModel->getUserId($_POST["username"]);
+    {//if ($_SESSION['loggedIn']) {
+	if (isset($_POST["submit_add_message"])){ 
+ 	    $userId = $this->messageModel->getUserId($_POST["username"]);
+	   // $userId = $_SESSION['userId'];  // if using session please select this line
 	    $toId = $this->messageModel->getUserId($_POST["toname"]);
 	    $this->messageModel->addMessage($userId, $toId, $_POST["content"]);
 	}
 
 	header('Location: ' . URL . 'home');
+     //}
     }
+
+   public function showMessage()
+   {
+	//if ($_SESSION['loggedIn'] {
+	    if (isset($_POST["submit_show_message"])) {
+		$userId =  $this->messageModel->getUserId($_POST["username"]);
+		
+		//$userId = $_SESSION['userId'];  // if using session please select this line
+	        $messages = $this->messageModel->showMessage($userId);
+		
+
+	       require APP . 'view/users/showMessage.php'; 
+	    }
+	//}
+   }
 }
 ?>
 
