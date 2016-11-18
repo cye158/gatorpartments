4a024c249cfb2d147d5f654bb3f0bd13ec40fd5f
mbain <mbain@mail.sfsu.edu>
2016-11-17 23:29:17 -0800

    trying something

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html lang="en"><head>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
-    <title>Real estate - Bootstrap</title>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
 
-	<link id="switch_style" href="<?php echo URL;?>css/real_estate.css" rel="stylesheet">
-	<link href="<?php echo URL;?>css/theme.css" rel="stylesheet">
-    <!--[if lt IE 9]>
-		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-
-</head>
-
-<body>
     <div class="container">
 		<div class="row">
 	<?php require APP . 'view/_templates/navbar.php'; ?>
