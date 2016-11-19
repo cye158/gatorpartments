dc9c8b90b5fb562530b7b296bde49f0935ccfde3
Alvin Lu <alvinlu@mail.sfsu.edu>
2016-11-19 00:55:23 -0800

    testing sample form

--- application/controller/home.php
+++ application/controller/home.php
@@ -35,6 +35,13 @@ class Home extends Controller
         require APP . 'view/home/listings.html';
         require APP . 'view/_templates/footer.php';
     }
+    public function listing_form()
+    {
+        // load views
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/home/listing_form.php';
+        require APP . 'view/_templates/footer.php';
+    }
     public function index()
     {
 
