87ecfa276ec7ae8ff9f0ac231c276104ab2017fe
Alvin Lu <alvinlu@mail.sfsu.edu>
2016-11-24 22:21:59 -0800

    testing styling

--- application/view/home/listing_form.php
+++ application/view/home/listing_form.php
@@ -104,26 +104,9 @@ function test_input($data) {
 <h2>Post A Listing</h2>
 <p><span class="error">* required field.</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  <!--
-  Name: <input type="text" name="name" value="<?php echo $name;?>">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
-  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-  <span class="error">* <?php echo $emailErr;?></span>
-  <br><br>
-  Website: <input type="text" name="website" value="<?php echo $website;?>">
-  <span class="error"><?php echo $websiteErr;?></span>
-  <br><br>
-  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-  <br><br>
-  Gender:
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-  <span class="error">* <?php echo $genderErr;?></span>
-  <br><br>
-  -->
-  <h3>Location</h3>
 
+  <h3>Location</h3>
+<!--
     <listinglabel>Address: </listinglabel><input type="text" name="address" value="<?php echo $address;?>"></>
     <span class="error">* <?php echo $addressErr;?></span>
     <br><br>
@@ -248,6 +231,16 @@ function test_input($data) {
 
     <listinglabel>Comment: </listinglabel><br><textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
     <br><br>
+-->
+<table>
+  <tr>
+    <th>Adress</th>
+  </tr>
+  <tr>
+    <td><input type="text" name="address" value="<?php echo $address;?>"></></td>
+  </tr>
+</table>
+
 
   <!-- button for form submission usage-->
   <input type="submit" name="submit" value="Submit">
@@ -256,6 +249,7 @@ function test_input($data) {
 
 <!-- echo user input -->
 <h2>PHP Form Validation Example</h2>
+<h3>currently only showing address, city, state, & zip code</h3>
 <?php
 echo "<h2>Your Input:</h2>";
 echo $address;
