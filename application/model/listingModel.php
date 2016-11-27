76640dda828bf166cedce22b371683c423e3a8a9
Nhan <nhan4@msn.com>
2016-11-27 03:06:19 +0000

    Update listingModel and made userModel cleaner

--- application/model/listingModel.php
+++ application/model/listingModel.php
@@ -1,48 +1,63 @@
-<?php 
+<?php
 
 class ListingModel
 {
-  //Creates db connection
+  //Connectes and creates db object
   function __construct($db)
   {
-    try{
+    try {
       $this->db = $db;
-    } catch (PDOException $e) {
-      exit('Database connection could not be established.');
+        } catch (PDOException $e) {
+          exit('Database connection could not be established.');
     }
   }
 
-  //gets all listings from DB
+  //Gets all listings in the 'listing' table in the DB
   public function getAllListing()
   {
-      $sql = "SELECT * FROM listing";
-      $query = $this->db->prepare($sql);
-      $query->execute();
-      return $query->fetchAll();
+    $sql = "SELECT * FROM listing";
+    $query = $this->db->prepare($sql);
+    $query->execute();
+    return $query->fetchAll();
+   }
 
-  }
-
-  //gets any listings that has keywords
+  //Gets listings if a keyword is matched
+  //Used in search button
   public function getListing($search)
   {
     $search = "%$search%";
-    $sql = "SELECT * FROM listing WHERE CONCAT_WS('', address_1, address_2, city, zip_code, title, description, room_size, price) LIKE :search";
+    $sql = "SELECT * FROM listing WHERE CONCAT_WS('', address_1, city, zip_code, title, description, room_asize, price) LIKE :search";
     $query = $this->db->prepare($sql);
     $query->bindParam(':search', $search);
     $query->execute();
     return $query->fetchAll();
   }
 
-  //Finds a listing by its ID numbers
+  //Gets listing by listing's id
   public function getListingById($id)
   {
     $sql = "SELECT * FROM listing WHERE id=:id";
     $query = $this->db->prepare($sql);
     $query->bindParam(':id', $id);
     $query->execute();
-    return $query->fetchAll();
+    return $query->fetch();
   }
 
+  //Given strings, it will put it in an array
+  public function getImagesArray($string)
+  {
+    $images = explode(" ",$string);
+    return $images;
+  }
+
+  //Gets all listings in random order
+  public function getAllListingRandom()
+  {
+    $sql = "SELECT * FROM listing ORDER BY RAND()";
+    $query = $this->db->prepare($sql);
+    $query->execute();
+    return $query->fetchAll();
+  }
 
 }
 ?>
