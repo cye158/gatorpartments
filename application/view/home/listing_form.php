eefaa291c408860e5321ab491c8ac9098809a1d9
Alvin Lu <alvinlu@mail.sfsu.edu>
2016-11-27 21:34:14 -0800

    posting is breaking

--- application/view/home/listing_form.php
+++ application/view/home/listing_form.php
@@ -3,30 +3,31 @@
   $addressErr = $cityErr = $stateErr = $zip_codeErr = $complexErr = $termErr = $priceErr = $square_feetErr = $number_of_bedroomErr = $number_of_bathErr = $utilities_providedErr = $building_accomodationErr = $building_restrictionErr = $availability_dateErr = "";
   $address = $city = $state = $zip_code = $complex = $term = $price = $square_feet = $number_of_bedroom = $number_of_bath = $utilities_provided = $building_accomodation = $building_restriction = $availability_date = "";
 
-  function display()
-  {
-    //echo "hello".$_POST["studentname"];
-    echo $address;
-  }
+
   if(isset($_POST['Post']))
   {
-    $address = $_POST['address'];
-    echo $address;
-    //display();
+
+    if (empty($_POST["address"]))
+    {
+    $addressErr = "address is required";
+    } else {
+      $address = $_POST['address'];
+
+      // check if address only contains letters, numbers, and whitespace
+      if (!preg_match("/^[a-zA-Z0-9 ]*$/",$address)) {
+      $addressErr = "Only letters, numbers, and white space allowed";
+      } else {
+        echo $address;
+      }
+    }
   }
-  function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
 ?>
 
 <h2>Post A Listing</h2>
 
 <form method="post" action="<?php echo URL;?>home/listing_form">
 
-  <input type="text" name="address">
+  <input type="text" name="address"></><span class="error">* <?php echo $addressErr;?></span>
 
   <!-- button for form submission usage-->
   <input type="submit" name="Post" value="Post">
