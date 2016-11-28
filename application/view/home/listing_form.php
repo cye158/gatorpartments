e357d4a358a64e34c7e04b858761f085c0ad768f
Alvin Lu <alvinlu@mail.sfsu.edu>
2016-11-27 20:55:43 -0800

    testing the final damn time

--- application/view/home/listing_form.php
+++ application/view/home/listing_form.php
@@ -3,7 +3,14 @@
   $addressErr = $cityErr = $stateErr = $zip_codeErr = $complexErr = $termErr = $priceErr = $square_feetErr = $number_of_bedroomErr = $number_of_bathErr = $utilities_providedErr = $building_accomodationErr = $building_restrictionErr = $availability_dateErr = "";
   $address = $city = $state = $zip_code = $complex = $term = $price = $square_feet = $number_of_bedroom = $number_of_bath = $utilities_provided = $building_accomodation = $building_restriction = $availability_date = "";
 
-
+  function display()
+  {
+    echo "hello".$_POST["studentname"];
+  }
+  if(isset($_POST['Post']))
+  {
+     display();
+  }
 ?>
 
 <h2>Post A Listing</h2>
@@ -16,14 +23,3 @@
   <input type="submit" name="Post" value="Post">
 
 </form>
-
-<?php
-function display()
-{
-  echo "hello".$_POST["studentname"];
-}
-if(isset($_POST['Post']))
-{
-   display();
-}
-?>
