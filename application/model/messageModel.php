5bc96f6807f1100ca3963d6a07445df2d20a686e
Xuan Zhang <xzhang8@mail.sfsu.edu>
2016-11-28 21:38:15 +0000

    modify messageModel.php
    1. add listingId in add message
    
    2. add functions showing all messages from others
    
    3. add functions showing all messages sent to others

--- application/model/messageModel.php
+++ application/model/messageModel.php
@@ -1,34 +1,71 @@
 <?php
-/*
-*	This model is used to send and receive messages
-*
-*
-*
-*/
 class MessageModel
-{   
-    //Create Db Connection
+{
+	//Create Db Connection
     function __construct($db)
     {
-        try{
-            $this->db = $db;
-        }  catch (PDOEXCEPTION $e){
-           exit('Database connection could not be established.'');
-        }
+	try {
+	    $this->db = $db;
+	} catch (PDOEXCEPTION $e) {
+	    exit('Database connection could not be established.');
+	}
     }
-    
+
+    public function getUserId($username)
+    {
+	$sql = "SELECT id from users where username =:username";
+	$query = $this->db->prepare($sql);
+	$parameters = array(':username' => $username);
+
+	$query->execute($parameters);
+
+	return $query->fetch()->id;
+    }
+
     //creates a message onto message table in DB
     //Takes in user's id, receipient's id, and message content
-    public function addMessage($userId, $toId, $content)
+    public function addMessage($userId, $toId, $content, $listingId)
     {
-        $sql = "INSERT INTO message (userId, toId, content) VALUES (:userId, :toId, :content)";
-        $query = $this->db->prepare($sql);
-        $parameters = array(':userId' => $userId, ':toId' => $toId, ':content' => $content);
-        $query->execute($parameters);
+	$sql = "INSERT INTO messages (userId, toId, content, listingId) VALUES (:userId, :toId, :content, :listingId)";
+	$query = $this->db->prepare($sql);
+	$parameters = array(':userId' => $userId, ':toId' => $toId, ':content' => $content, ':listingId' => $listingId);
 
+	$query->execute($parameters);
     }
+	
+    public function getUsername($userId)
+    {
+	$sql =  "select username from users where id = :id";
+	$query = $this->db->prepare($sql);
+	$parameters = array(':id' => $id);
+
+	$query->execute($parameters);
+   	return $query->fetch()->username;
+    } 
+
+   public function showToMessage($userId)
+   {
+        $sql = "select u.username, m.content, m.listingId from users u, messages m where u.id = m.toId and m.userId = :userId";
+	$query = $this->db->prepare($sql);
+        $parameters = array(':userId' => $userId);
+
+	$query->execute($parameters);
+	return $query->fetchAll(); 
+    }
+
+   public function showFromMessage($userId)
+   {
+	$sql = "select u.username, m.content, m.listingId from users u, messages m where u.id = m.userId and m.toId = :userId";
+	$query = $this->db->prepare($sql);
+	$parameters =  array(':userId' => $userId);
+
+	$query->execute($parameters);
+	return $query->fetchAll();
+   }
 
 
 }
 ?>
+
+
 
