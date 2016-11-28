4361b62b8a79a65074e29cb6a735b7d246e48bb6
Alvin Lu <alvinlu@mail.sfsu.edu>
2016-11-27 20:42:23 -0800

    changing hierarchy of code

--- application/view/home/listing_form.php
+++ application/view/home/listing_form.php
@@ -1,21 +1,12 @@
 <?php
+  //defined variables for gatorpartment post
+  $addressErr = $cityErr = $stateErr = $zip_codeErr = $complexErr = $termErr = $priceErr = $square_feetErr = $number_of_bedroomErr = $number_of_bathErr = $utilities_providedErr = $building_accomodationErr = $building_restrictionErr = $availability_dateErr = "";
+  $address = $city = $state = $zip_code = $complex = $term = $price = $square_feet = $number_of_bedroom = $number_of_bath = $utilities_provided = $building_accomodation = $building_restriction = $availability_date = "";
 
-//defined variables for gatorpartment post
-$addressErr = $cityErr = $stateErr = $zip_codeErr = $complexErr = $termErr = $priceErr = $square_feetErr = $number_of_bedroomErr = $number_of_bathErr = $utilities_providedErr = $building_accomodationErr = $building_restrictionErr = $availability_dateErr = "";
-$address = $city = $state = $zip_code = $complex = $term = $price = $square_feet = $number_of_bedroom = $number_of_bath = $utilities_provided = $building_accomodation = $building_restriction = $availability_date = "";
 
-function display()
-{
-  echo "hello".$_POST["studentname"];
-}
-if(isset($_POST['Post']))
-{
-   display();
-}
 ?>
 
 <h2>Post A Listing</h2>
-
 
 <form method="post" action="listing_form.php">
 
@@ -25,3 +16,14 @@ if(isset($_POST['Post']))
   <input type="submit" name="Post" value="Post">
 
 </form>
+
+<?php
+function display()
+{
+  echo "hello".$_POST["studentname"];
+}
+if(isset($_POST['Post']))
+{
+   display();
+}
+?>
