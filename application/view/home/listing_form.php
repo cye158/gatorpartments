308bd7583b590441d922dab1a33e6898f958835c
Alvin Lu <alvinlu@mail.sfsu.edu>
2016-12-03 15:45:16 -0800

    allowing for dividers

--- application/view/home/listing_form.php
+++ application/view/home/listing_form.php
@@ -320,89 +320,9 @@
         <label class="listinglabel">Zip Code *</label><input class="listingtext" type="text" name="zip_code"></>
         <br><hr class="listingruler">
   </div>
-
-
-<!-- city state zipcode table -->
-<table class="lf" border="1">
-  <tr>
-    <th class="lf">City *</th>
-    <th class="lf">State *</th>
-    <th class="lf">Zip Code *</th>
-  </tr>
-  <tr>
-    <td class="lf">
-      <input type="text" name="city"></>
-    </td>
-    <td class="lf">
-      <select name="state">
-        <option value=""></option>
-        <option value="AL">AL</option>
-        <option value="AK">AK</option>
-        <option value="AZ">AZ</option>
-        <option value="AR">AR</option>
-        <option value="CA">CA</option>
-        <option value="CO">CO</option>
-        <option value="CT">CT</option>
-        <option value="DE">DE</option>
-        <option value="FL">FL</option>
-        <option value="GA">GA</option>
-        <option value="HI">HI</option>
-        <option value="ID">ID</option>
-        <option value="IL">IL</option>
-        <option value="IN">IN</option>
-        <option value="IA">IA</option>
-        <option value="KS">KS</option>
-        <option value="KY">KY</option>
-        <option value="LA">LA</option>
-        <option value="ME">ME</option>
-        <option value="MD">MD</option>
-        <option value="MA">MA</option>
-        <option value="MI">MI</option>
-        <option value="MN">MN</option>
-        <option value="MS">MS</option>
-        <option value="MO">MO</option>
-        <option value="MT">MT</option>
-        <option value="NE">NE</option>
-        <option value="NV">NV</option>
-        <option value="NH">NH</option>
-        <option value="NJ">NJ</option>
-        <option value="NM">NM</option>
-        <option value="NY">NY</option>
-        <option value="NC">NC</option>
-        <option value="ND">ND</option>
-        <option value="OH">OH</option>
-        <option value="OK">OK</option>
-        <option value="OR">OR</option>
-        <option value="PA">PA</option>
-        <option value="RI">RI</option>
-        <option value="SC">SC</option>
-        <option value="SD">SD</option>
-        <option value="TN">TN</option>
-        <option value="TX">TX</option>
-        <option value="UT">UT</option>
-        <option value="VT">VT</option>
-        <option value="VA">VA</option>
-        <option value="WA">WA</option>
-        <option value="WV">WV</option>
-        <option value="WI">WI</option>
-        <option value="WY">WY</option>
-      </select></>
-    </td>
-    <td class="lf">
-      <input type="text" name="zip_code"></>
-    </td>
-  </tr>
-</table>
-
-<!-- complex term price table -->
-<table class="lf" border="1">
-  <tr>
-    <th class="lf">Complex *</th>
-    <th class="lf">Term *</th>
-    <th class="lf">Price *</th>
-  </tr>
-  <tr>
-    <td class="lf">
+  <div>
+    <!-- complex term price -->
+    <label class="listinglabel">Complex *</label>
       <select name="complex">
         <option value=""></option>
         <option value="apartment">apartment</option>
@@ -410,19 +330,15 @@
         <option value="single home">single home</option>
         <option value="town home">town home</option>
       </select>
-    </td>
-    <td class="lf">
+    <label class="listinglabel">Term *</label>
       <select name="term">
         <option value=""></option>
         <option value="lease">lease</option>
         <option value="month to month">month to month</option>
       </select>
-    </td>
-    <td class="lf">
-      <input type="text" name="price"></>
-    </td>
-  </tr>
-</table>
+    <label class="listinglabel">Price *</label><input class="listingtext" type="text" name="price"></>
+  </div>
+
 
 <!-- square feet bedroom bath -->
 <table class="lf" border="1">
