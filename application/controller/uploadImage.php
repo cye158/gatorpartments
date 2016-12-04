0d3fa2975692005d484f18cebc4247f24f802a2d
Ray Liu <rliu123@mail.sfsu.edu>
2016-12-04 01:45:04 +0000

    created simple UI for testing image upload

---
+++ application/controller/uploadImage.php
@@ -0,0 +1,9 @@
+<?php
+  class uploadImage extends Controller {
+    public function index() {
+      require APP . 'view/_templates/header.php';
+      require APP . 'view/uploadImage/index.php';
+      require APP . 'view/_templates/footer.php';
+    }
+  }
+?>
