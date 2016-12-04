f186e39a349ee87eaff9ae0aa9a4d568070672ca
Ray Liu <rliu123@mail.sfsu.edu>
2016-12-04 03:37:19 +0000

    adjusted the UI for uploadImage testing

--- application/view/uploadImage/index.php
+++ application/view/uploadImage/index.php
@@ -1,9 +1,12 @@
-<div class="container">
+<!DOCTYPE html>
+<html>
+<body>
 
-  <form action="upload.php" method="post" enctype="multipart/form-data">
+  <form action="<?php echo URL . 'uploadImage/upload'?>" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
+    <input type="submit" value="Upload Image">
   </form>
 
-</div>
+</body>
+</html>
