8b56d04e0c55d8d35a328f7f9c7cdbc752115c66
Alvin Lu <alvinlu@mail.sfsu.edu>
2016-12-03 17:15:09 -0800

    echo red

--- application/view/home/listing_form.php
+++ application/view/home/listing_form.php
@@ -241,6 +241,10 @@
 <h2>Post A Listing</h2>
 
 <!-- error messages -->
+<?php
+  echo <font color="red">$address1Err."<br>"</font>;
+
+?>
 <?php echo nl2br($address1Err);?>
 <?php echo nl2br($address2Err);?>
 <?php echo nl2br($cityErr);?>
