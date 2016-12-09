69cc0d07847702d1e753f3814e447be525b2b060
Clarence Enriquez <cye0105@mail.sfsu.edu>
2016-12-09 15:39:35 -0800

    modifying layout and color scheme; adding search icon png

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -24,16 +24,16 @@
 		<div class="row">
 
 			<!-- start header -->
-			<div class="row" style="height: 4em;">
+			<div class="row" style="height: 4em;"><!-- start header -->
 				<div class="col-sm-4 col-xs-6 logo" style="height: inherit;">
 					<a href="<?php echo URL;?>home/index">
 						<div class="row">
 							<div class="col-sm-3 hidden-xs logo-img">
-								<img src="<?php echo URL;?>css/images/Home-green-48.png" alt=""/>
+								<img src="<?php echo URL;?>css/images/Home-green-48.png" alt="">
 
 							</div>
 							<div class="col-sm-9 logo-text">
-								<h1><small>Team 4</small><br />Gatorpartments</h1>
+								<h1><small>Team 4</small><br>Gatorpartments</h1>
 							</div>
 						</div>
 					</a>
@@ -49,11 +49,11 @@
 			<div class="row">
 				<div class="col-sm-12" style="height: 3em;">
 
-					<nav class="navbar navbar-inverse" role="navigation" style="border-radius: 5px; border: 1px solid grey;">
-						<div class="navbar-inner" style="border-radius: 5px;height: 2.85em; border-bottom: 4px solid grey;">
+					<nav class="navbar navbar-inverse" role="navigation" style="border-radius: 0px;border: 2px solid #444444;">
+						<div style="border-radius: 0px;height: 2.85em;border: 2px solid #444444;" class="navbar-inner">
 							<!-- Brand and toggle get grouped for better mobile display -->
 							<div class="navbar-header">
-								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-ex1-collapse">
 									<span class="sr-only">Toggle navigation</span>
 									<span class="icon-bar"></span>
 									<span class="icon-bar"></span>
@@ -63,64 +63,66 @@
 							</div>
 
 							<!-- Collect the nav links, forms, and other content for toggling -->
-							<div class="collapse navbar-collapse navbar-ex1-collapse">
+							<div class="navbar-collapse navbar-ex1-collapse collapse">
 								<ul class="nav navbar-nav center">
 
 									<!-- Inbox button -->
-									<li><a href="<?php echo URL;?>home/messaging" class="first" style="width: 5em; padding-top: 0.6em; font-size: 1.25em; color: black; border-right: 2px solid grey; border-left: none;">Inbox</a></li>
-									<!-- /Inbox button -->
+									<li><a href="<?php echo URL;?>home/messaging" class="first" style="height: 2.3em; width: 5em; padding-top: 0.5em; font-size: 1.25em; border-right: 2.5px solid #444444; border-left: none;
 
-									<!-- Post a Listing -->
-									<li><a href="<?php echo URL;?>home/listing_form" style="width: 5em; padding-top: 0.6em; font-size: 1.25em;color: black; border-right: 2px solid grey; border-left: none;">Post</a></li>
-									<!-- /Post a Listing -->
+										">Inbox</a></li>
+										<!-- /Inbox button -->
 
-									<!-- Search bar -->
-									<li>
-										<a style=" border-right: 2px solid grey; border-left: none;height: 2.7em; padding-left: 1em; padding-right: 1em;margin-top: -1px;">
-											<form name="searchbox" method="post" action="<?php echo URL;?>home/listings" style="padding-left: 0.25em;">
-												<input type="search" name="searchBarInput" textarea="" placeholder=" Search by location (eg. 94132, or Holloway Avenue)" size="60" maxlength="600px" style="border-radius: 7.5px;color: black; font-size: 1.1em;padding-bottom: .225em;">
-												<input type="submit" value="Search" name="submitSearch" style="height: 1.5em; background-color: darkgrey;border-radius: 9px;font-size: 1.25em;">
-											</form>
-										</a>
-									</li>
-									<li></li>
-								</ul>
+										<!-- Post a Listing -->
+										<li><a href="<?php echo URL;?>home/listing_form" style="height: 2.3em; width: 5em; padding-top: 0.5em; font-size: 1.25em; border-right: 2.5px solid #444444; border-left: none;">Post</a></li>
 
-								<ul class="nav navbar-nav navbar-right" style="height: 2.75em;">
+										<!-- Search bar -->
+										<li>
+											<a style="height: 2.85em;border-right: 2.5px solid #444444;border-left: none;padding-left: 1em;padding-right: 1em;">
+												<form name="searchbox" method="post" action="<?php echo URL;?>home/listings">
+													<input type="search" name="searchBarInput" textarea="" placeholder=" Search by zip, or location (eg. 94132, or Holloway Avenue)" size="60" maxlength="600px" style="border-radius: 7.5px;color: black; font-size: 1.1em;padding-bottom: .225em; background-color: #dddddd;">
+													<input type="submit" value="Search" name="submitSearch" style="height: 1.5em;background-color: lightslategrey;border-radius: 9px;font-size: 1.25em;">
+												</form>
+											</a>
+										</li>
+										<li></li>
+									</ul>
 
-									<li class="dropdown">
-										<a class="dropdown-toggle" href="#" data-toggle="dropdown" style="height: 2.05em; width: 5em; padding-top: 0.6em; font-size: 1.25em; color: black; border-right: none; border-left: 2px solid grey;">Sign Up<strong class="caret"></strong></a>
-										<div class="dropdown-menu">
-											<form action="<?php echo URL;?>user/register" method="post">
-												<input type="text" name="username" placeholder="Username" size="30">
-												<input type="text" name="name" placeholder="Full name" size="30">
-												<input type="text" name="email" placeholder="Email" size="30">
-												<input type="password" name="password" placeholder="Password" size="30">
+									<ul class="nav navbar-nav navbar-right" style="height: 2.75em;">
 
-												<input class="btn btn-primary" type="submit" name="commit" value="Sign Up">
-											</form>
-										</div>
-									</li>
+										<li class="dropdown">
+											<a class="dropdown-toggle center" href="#" data-toggle="dropdown" style="height: 2.3em; width: 5.5em; padding-top: 0.6em; font-size: 1.25em; border-right: none; border-left: 2.5px solid #444444;">Sign Up<strong class="caret"></strong></a>
+											<div class="dropdown-menu">
+												<form action="<?php echo URL;?>user/register" method="post">
+													<input type="text" name="username" placeholder="Username" size="30">
+													<input type="text" name="name" placeholder="Full name" size="30">
+													<input type="text" name="email" placeholder="Email" size="30">
+													<input type="password" name="password" placeholder="Password" size="30">
 
-									<li class="dropdown">
-										<a class="dropdown-toggle" href="#" data-toggle="dropdown" style="width: 5em; padding-top: 0.6em; font-size: 1.25em; color: black; border-right: none; border-left: 2px solid grey;">Sign In <strong class="caret"></strong></a>
-										<div class="dropdown-menu">
-											<form action="<?php echo URL;?>user/authenticateLogin" method="post">
-												<input type="text" name="username" placeholder="Username or email" size="30"><br class="visible-xs">
-												<input type="password" name="password" placeholder="Password" size="30"><br class="visible-xs">
-												<input id="remember_me" type="checkbox" name="remember_me" value="1">
-												<label class="string optional"> Remember me</label>
+													<input class="btn btn-primary" type="submit" name="commit" value="Sign Up">
+												</form>
+											</div>
+										</li>
 
-												<input class="btn btn-primary" type="submit" name="commit" value="Sign In">
-											</form>
-										</div>
-									</li>
+										<li class="dropdown">
+											<a class="dropdown-toggle center" href="#" data-toggle="dropdown" style="height: 2.3em; width: 5.5em; padding-top: 0.6em; font-size: 1.25em; border-right: none; border-left: 2.5px solid #444444;">Sign In <strong class="caret"></strong></a>
+											<div class="dropdown-menu">
+												<form action="<?php echo URL;?>user/authenticateLogin" method="post">
+													<input type="text" name="username" placeholder="Username or email" size="30"><br class="visible-xs">
+													<input type="password" name="password" placeholder="Password" size="30"><br class="visible-xs">
+													<input id="remember_me" type="checkbox" name="remember_me" value="1">
+													<label class="string optional"> Remember me</label>
 
-								</ul>
+													<input class="btn btn-primary" type="submit" name="commit" value="Sign In">
+												</form>
+											</div>
+										</li>
 
-							</div><!-- /.navbar-collapse -->
-						</div>
-					</nav>
+									</ul>
 
+								</div>
+								<!-- /.navbar-collapse -->
+							</div>
+						</nav>
+
+					</div>
 				</div>
-			</div>
