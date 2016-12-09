4d446538d0a4b20e520d33fd6cc7b3a77ed86f4c
mbain <mbain@mail.sfsu.edu>
2016-12-08 19:41:06 -0800

    Attempting to resolve syntax errors.

--- application/model/userModel.php
+++ application/model/userModel.php
@@ -56,6 +56,7 @@ class UserModel {
   // return false, outputs error message
   echo "<meta http-equiv=\"refresh\" content=\"5;url=".$_SERVER['HTTP_REFERER']."\"/>";
   echo "Error, username or password does not match";
+}
   */
   //Compares the argument's username and password in DB
   //If matches, we load user info into sessions for persistent data
