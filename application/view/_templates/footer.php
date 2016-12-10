736d52a23a0fcd3875d8be40c4188dbce7412f53
Clarence Enriquez <cye0105@mail.sfsu.edu>
2016-12-09 18:42:26 -0800

    trying new google map key

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -29,8 +29,8 @@
     <script type="text/javascript" src="<?php echo URL;?>js/search-result.js"></script>
 
 	  <link  href="<?php echo URL;?>css/maps.css" rel="stylesheet">
-    <script type="text/javascript" src="<?php echo URL;?>js/maps.js"></script>
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkUOdZ5y7hMm0yrcCQoCvLwzdM6M8s5qk&callback=initMap"></script>
+    <script type="text/javascript" src="<?php echo URL;?>js/maps.js"></script>
 
 </body>
 </html>
