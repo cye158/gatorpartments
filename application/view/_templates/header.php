a331a8c83636d31ffa01bdbaa6643782567fa18f
Clarence Enriquez <cye0105@mail.sfsu.edu>
2016-12-13 04:09:52 -0800

    fixed resize/rescale of navbar

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -22,13 +22,12 @@
 </head>
 
 <body>
-
-	<div class="container" style="height: 50em;">
+	<div class="container" style="padding-left: 17px; padding-right: 16px;">
 		<div class="row">
 
 			<!-- start header -->
-			<div class="row" style="height: 4em;">
-				<div class="col-sm-4 col-xs-6 logo" style="height: inherit;">
+			<div class="row">
+				<div class="col-sm-4 col-xs-6 logo">
 					<a href="<?php echo URL;?>home/index">
 						<div class="row">
 							<div class="col-sm-3 hidden-xs logo-img">
@@ -44,8 +43,8 @@
 
 				<!-- Customer Service -->
 				<div class="col-sm-4 col-xs-6 customer_service pull-right text-right">
-					<h4 class="phone"><span class="hidden-xs">Hensil Hall 206 </span>CSC 648.01</h4>
-					<h4><small>M: 5:35PM - 8:25PM<br>T-Su: Closed</small></h4>
+					<h4 class="phone"><span>Hensil Hall 206 CSC 648.01</span></h4>
+					<h4><small>M: 5:35PM - 8:25PM; T-Su: Closed</small></h4>
 				</div>
 				<!-- /Customer Service -->
 
@@ -54,7 +53,7 @@
 
 			<!-- start nav -->
 			<div class="row">
-				<div class="col-sm-12" style="height: 3em;">
+				<div class="col-sm-12" style="">
 
 					<nav class="navbar navbar-inverse" role="navigation">
 						<div class="navbar-inner">
@@ -71,7 +70,7 @@
 							</div>
 
 							<!-- Collect the nav links, forms, and other content for toggling -->
-							<div class="navbar-collapse navbar-ex1-collapse collapse">
+							<div class="navbar-collapse navbar-ex1-collapse collapse" style="height: 1px;">
 
 								<ul class="nav navbar-nav center">
 
@@ -83,28 +82,36 @@
 									<li><a id="navbar-btn" href="<?php echo URL;?>user/checkLoginStatus">Post</a></li>
 									<!-- /Post a Listing -->
 
-									<!-- Search bar -->
+									<!-- Search -->
 									<li>
-										<a style="height: 2.85em;border-right: 2.5px solid #444444;border-left: none;padding-left: 1em;padding-right: 1em;">
-											<form name="searchbox" method="post" action="<?php echo URL;?>home/listings">
-												<input type="search" name="searchBarInput" textarea="" placeholder="Enter location (eg. 94132, or 19th Avenue)" size="41" maxlength="37.5px" style="border-radius: 2px;color: black;font-size: 1.1em;padding-top: .225em;background-color: #dddddd;">
-												<input type="submit" value="Search" name="submitSearch" style="color: #eee;background-color: #809262;border-radius: 2px;font-size: 1.125em;">
-											</form>
-										</a>
-									</li>
+									<a class="visible-xs" style="border:0; padding-top: 0;">
+										<form name="searchbox" method="post" action="<?php echo URL;?>home/listings">
+											<input id="search-input-v" type="search" name="searchBarInput" textarea="" placeholder="Enter location (eg. 94132, or 19th Avenue)" size="45" maxlength="50">
+											<input id="search-btn-v" value="Search" name="submitSearch" type="submit">
+										</form>
+									</a>
+
+									<a class="hidden-xs" style="border: 0;">
+										<form name="searchbox" method="post" action="<?php echo URL;?>home/listings">
+											<input id="search-input-h" type="search" name="searchBarInput" textarea="" placeholder="Enter location (eg. 94132, or 19th Avenue)" size="48" maxlength="50">
+
+											<input id="search-btn-h" type="submit" value="Search" name="submitSearch" >
+										</form>
+									</a>
+								</li>
 									<!-- /Search -->
 
 								</ul>
 
 
-								<ul class="nav navbar-nav navbar-right" style="height: 2.75em;">
+								<ul class="nav navbar-nav navbar-right center">
 
 									<!-- Login -->
-									<li><a href="<?php echo URL . 'user/login';?>">Log in</a></li>
+									<li><a id="navbar-btn" href="<?php echo URL . 'user/login';?>">Log in</a></li>
 									<!-- /Login -->
 
 									<!-- Register -->
-									<li><a href="<?php echo URL . 'user/register';?>">Register</a></li>
+									<li><a id="navbar-btn" href="<?php echo URL . 'user/register';?>">Register</a></li>
 									<!-- /Register -->
 
 								</ul>
