ee3c39f2d4c5f7f1d162698c38479128f85c4b73
Clarence Enriquez <cye0105@mail.sfsu.edu>
2016-12-15 23:21:19 -0800

    trying to do session for search

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -20,11 +20,6 @@
 	<![endif]-->
 </head>
 
-<!-- php -->
-$location= "";
-
-<!-- /php -->
-
 <body>
 	<div class="container cont">
 		<div class="row">
@@ -91,8 +86,29 @@ $location= "";
 									<li>
 										<a class="hidden-xs search" style="margin-top: 0.325em;">
 											<form name="searchbox" method="post" action="<?php echo URL;?>home/listings">
-												<input class="search-input-h" type="search" name="searchBarInput" textarea="" placeholder="Enter location (eg. 94132, or 19th Avenue)" size="48" maxlength="50">
+
+												<!-- php -->
+												<?php
+												session_start();
+												if ($_POST['submitSearch']){}
+													if ( ($_POST['location'] != "") && ($_POST['location'] != "0") && (is_numeric('location') || is_string('location')) ){
+														$_SESSION['location_session'] = $_POST['location'];
+													}
+												}
+												$location_session = $_SESSION['location_session']
+												echo ($location_session);
+												?>
+												<!-- /php -->
+												<input class="search-input-h" type="search" name="location" placeholder="Enter location (eg. 94132, or 19th Avenue)" size="48" maxlength="50" value="<?php
+												if ($location_session == ""){
+													echo ("0");
+												}else {
+													echo ($location_session);
+												}
+												?>
+												"/>
 												<input class="search-btn-h" type="submit" value="Search" name="submitSearch">
+
 											</form>
 										</a>
 									</li>
