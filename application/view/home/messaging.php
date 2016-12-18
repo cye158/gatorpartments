1d6eee3d97e93fbc23e94facfa74416452ed20b8
mbain <mbain@mail.sfsu.edu>
2016-12-18 14:45:41 -0800

    Fix to column content in inbox UI.

--- application/view/home/messaging.php
+++ application/view/home/messaging.php
@@ -1,61 +1,61 @@
 <?php
-  if(isset($_SESSION['isLandlord'])){
-    $inboxType = 'landlord';
-    echo("Landlord inbox.");
+if(isset($_SESSION['isLandlord'])){
+  $inboxType = 'landlord';
+  echo("Landlord inbox.");
+}
+elseif (isset($_SESSION['isStudent'])) {
+  $inboxType = 'student';
+  echo("Student inbox.");
+}
+else{
+  echo("No user type is defined.");
+}
+?>
+<hr />
+<div class="col-md-3">Property column<br>
+  <?php echo('<ul>');
+  $index = 0;
+  foreach($messages as $message){
+    echo('<a href="' . URL . 'messages/showMessageDetail' . $message->id . '" alt="Message ID' . $message->id . '"/>');
+    echo('<li>' . $listings[$index]->title . '</li>');
+    echo('</a>');
   }
-  elseif (isset($_SESSION['isStudent'])) {
-    $inboxType = 'student';
-    echo("Student inbox.");
-  }
-  else{
-    echo("No user type is defined.");
-  }
- ?>
+  echo('</ul>');
+  ?></div>
+  <div class="col-md-3">Message from column</div>
+  <div class="col-md-4">Message content column</div>
+  <table id="table">
+    <tr>
+      <th id="property">Property</th>
+      <th id="messsageFrom">Message from:</th>
+      <th id="selectMessage">Click to select message</th>
+    </tr>
+    <?php $index = 0; ?>
+    <?php foreach ($messages as $message) { ?>
 
- <div class="col-md-3">Property column</div>
- <?php echo('<ul>');
- $index = 0;
- foreach($messages as $message){
-   echo('<a href="' . URL . 'messages/showMessageDetail' . $message->id . '" alt="Message ID' . $message->id . '"/>');
-   echo('<li>' . $listings[$index]->title . '</li>');
-   echo('</a>');
- }
- echo('</ul>');
- ?>
- <div class="col-md-3">Message from column</div>
- <div class="col-md-4">Message content column</div>
-<table id="table">
-  <tr>
-    <th id="property">Property</th>
-    <th id="messsageFrom">Message from:</th>
-    <th id="selectMessage">Click to select message</th>
-  </tr>
-  <?php $index = 0; ?>
-  <?php foreach ($messages as $message) { ?>
+      <tr>
+        <td headers="property"><a onclick="displayMessage(2);"><?php echo $listings[$index]->title; ?></a></td>
+        <td headers="messageFrom" id="message0002"><a onclick="displayMessage(2);"><?php echo $messageUserNames[$index]; $index++; ?></a></td>
+        <td headers="selectMessage" id="message0001"><a href="<?php echo URL . 'messages/showMessageDetail/' . $message->id ;?>"  class="btn btn-primary">Display Message</a></td>
+      </tr>
+      <?php  } ?>
 
-  <tr>
-    <td headers="property"><a onclick="displayMessage(2);"><?php echo $listings[$index]->title; ?></a></td>
-    <td headers="messageFrom" id="message0002"><a onclick="displayMessage(2);"><?php echo $messageUserNames[$index]; $index++; ?></a></td>
-    <td headers="selectMessage" id="message0001"><a href="<?php echo URL . 'messages/showMessageDetail/' . $message->id ;?>"  class="btn btn-primary">Display Message</a></td>
-  </tr>
-  <?php  } ?>
+    </table>
+    <table>
+      <tr>
+        <td id="messageContent">
+        </td>
+      </tr>
+    </table>
 
-</table>
-<table>
-  <tr>
-    <td id="messageContent">
-    </td>
-  </tr>
-</table>
+    <script>
+    function displayMessage(messageID){
+      if(messageID==1){
+        document.getElementById("messageContent").innerHTML = "Content of message 1";
+      }
+      elseif(messageID==2){
+        document.getElementById("messageContent").innerHTML = "Conent of message 2";
+      }
 
-<script>
- function displayMessage(messageID){
-   if(messageID==1){
-     document.getElementById("messageContent").innerHTML = "Content of message 1";
-   }
-   elseif(messageID==2){
-     document.getElementById("messageContent").innerHTML = "Conent of message 2";
-   }
-
- };
-</script>
+    };
+    </script>
