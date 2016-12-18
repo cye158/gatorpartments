8daa12dc1e41e61e89e0a82359b7f10cb80ceee2
Clarence Enriquez <cye0105@mail.sfsu.edu>
2016-12-18 00:07:14 -0800

    adding listing number on thumbnails

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,15 +1,15 @@
 
 
 var locations = [
-      ['Bondi Beach', -33.890542, 151.274856, 4],
-      ['Coogee Beach', -33.923036, 151.259052, 5],
+      ['Bondi Beach', -33.890542, 151.274856, 5],
+      ['Coogee Beach', -33.923036, 151.259052, 4],
       ['Cronulla Beach', -34.028249, 151.157507, 3],
       ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
       ['Maroubra Beach', -33.950198, 151.259302, 1]
     ];
 
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
+      zoom: 5,
       center: new google.maps.LatLng(-33.92, 151.25),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
@@ -31,7 +31,7 @@ var locations = [
         }
       })(marker, i));
     }
-    
+
 function initMap() {
   var location = {lat: 37.723894, lng: -122.4782};
   var gmap = new google.maps.Map(document.getElementById('gmap'), {
