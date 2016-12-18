ae0d9903810b81f377a1e4576e9b687b3a4bd43b
Clarence Enriquez <cye0105@mail.sfsu.edu>
2016-12-17 16:47:25 -0800

    tryign to try googlemaps js

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -1,49 +1,49 @@
-    <div class="container">SFSU/FAU/Fulda
-    Software Engineering Project, Fall 2016. For Demonstration Only</div>
+<div class="container">SFSU/FAU/Fulda
+  Software Engineering Project, Fall 2016. For Demonstration Only</div>
 
-    <!-- JQuery Minified -->
-    <script type="text/javascript" src="<?php echo URL;?>js/jquery-1.11.0.min.js"></script>
-    <script type="text/javascript" src="<?php echo URL;?>js/response.min.js"></script>
-
-
-    <!-- Important Owl stylesheet -->
-    <link rel="stylesheet" href="<?php echo URL;?>js/owl-carousel/owl.carousel.css">
-    <!-- Default Theme -->
-    <link rel="stylesheet" href="<?php echo URL;?>js/owl-carousel/owl.theme.css">
-    <!-- Include js plugin -->
-    <script src="<?php echo URL;?>js/owl-carousel/owl.carousel.js"></script>
-
-    <script type="text/javascript" src="<?php echo URL;?>js/jquery.aw-showcase/jquery.aw-showcase.js"></script>
-    <link rel="stylesheet" href="<?php echo URL;?>js/jquery.aw-showcase/css/style.css" />
-
-    <script src="<?php echo URL;?>bootstrap/js/bootstrap.min.js"></script>
+  <!-- JQuery Minified -->
+  <script type="text/javascript" src="<?php echo URL;?>js/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="<?php echo URL;?>js/response.min.js"></script>
 
 
-    <link rel="stylesheet" href="<?php echo URL;?>js/badger/badger.min.css">
-    <script type="text/javascript" src="<?php echo URL;?>js/badger/badger.min.js"></script>
+  <!-- Important Owl stylesheet -->
+  <link rel="stylesheet" href="<?php echo URL;?>js/owl-carousel/owl.carousel.css">
+  <!-- Default Theme -->
+  <link rel="stylesheet" href="<?php echo URL;?>js/owl-carousel/owl.theme.css">
+  <!-- Include js plugin -->
+  <script src="<?php echo URL;?>js/owl-carousel/owl.carousel.js"></script>
 
-    <link rel="stylesheet" href="<?php echo URL;?>js/sticky/stick.min.css">
-    <script type="text/javascript" src="<?php echo URL;?>js/sticky/sticky.min.js"></script>
+  <script type="text/javascript" src="<?php echo URL;?>js/jquery.aw-showcase/jquery.aw-showcase.js"></script>
+  <link rel="stylesheet" href="<?php echo URL;?>js/jquery.aw-showcase/css/style.css" />
 
-    <script type="text/javascript" src="<?php echo URL;?>js/portamento-min.js"></script>
+  <script src="<?php echo URL;?>bootstrap/js/bootstrap.min.js"></script>
 
-    <script type="text/javascript" src="<?php echo URL;?>js/search-result.js"></script>
-    <script type="text/javascript" src="<?php echo URL;?>js/maps.js"></script>
-    <!-- google maps -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDbTAiniS56gZ1ZGGWK9oU1mZ2S0488wJs&callback=initMap" async defer></script>
-    <!-- /google maps -->
 
-    <!-- Begin Google Analytics code -->
-    <script>
+  <link rel="stylesheet" href="<?php echo URL;?>js/badger/badger.min.css">
+  <script type="text/javascript" src="<?php echo URL;?>js/badger/badger.min.js"></script>
+
+  <link rel="stylesheet" href="<?php echo URL;?>js/sticky/stick.min.css">
+  <script type="text/javascript" src="<?php echo URL;?>js/sticky/sticky.min.js"></script>
+
+  <script type="text/javascript" src="<?php echo URL;?>js/portamento-min.js"></script>
+
+  <script type="text/javascript" src="<?php echo URL;?>js/search-result.js"></script>
+  <script type="text/javascript" src="<?php echo URL;?>js/maps.js"></script>
+  <!-- google maps -->
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDbTAiniS56gZ1ZGGWK9oU1mZ2S0488wJs&callback=initMap"></script>
+  <!-- /google maps -->
+
+  <!-- Begin Google Analytics code -->
+  <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-88972547-1', 'auto');
   ga('send', 'pageview');
 
-</script>
-    <!-- End Google Analytics code -->
+  </script>
+  <!-- End Google Analytics code -->
 </body>
 </html>
