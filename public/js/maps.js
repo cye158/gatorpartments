d9c124f0e643095bdc5e8cc8a59805c80aa08794
Clarence Enriquez <cye0105@mail.sfsu.edu>
2016-12-18 03:50:45 -0800

    testing out maps

--- public/js/maps.js
+++ public/js/maps.js
@@ -38,74 +38,72 @@ function initMap() {
   /*- /map for index page only -*/
 
   /*- map for listings page only -*/
-    //-- listing array
-    var locations_list = [
-      //****- input eg. -> [ 'location txt', 'lat', 'lng', 'id#' ]
-      ['Park Merced Office', 37.7183, 122.4810, 1],
-      ['866 Junipero Serra Blvd', 37.722308, -122.473073, 2],
-      ['Building B', 37.725122, -122.476012, 3],
-      ['76 Denslowe Dr', 37.723501, -122.474658, 4],
-    ];
+  //-- listing array
+  var locations_list = [
+    //****- input eg. -> [ 'location txt', 'lat', 'lng', 'id#' ]
+    ['Park Merced Office', 37.7183, 122.4810, 1],
+    ['866 Junipero Serra Blvd', 37.722308, -122.473073, 2],
+    ['Building B', 37.725122, -122.476012, 3],
+    ['76 Denslowe Dr', 37.723501, -122.474658, 4],
+  ];
 
-    //-- center at SFSU
-    var map_list = new google.maps.Map(document.getElementById('gmap'), {
-      zoom: 15,
-      center: new google.maps.LatLng(37.723894, -122.4782),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+  //-- center at SFSU
+  var map_list = new google.maps.Map(document.getElementById('gmap'), {
+    zoom: 15,
+    center: new google.maps.LatLng(37.723894, -122.4782),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var infowindow = new google.maps.InfoWindow();
+
+  var marker_list;
+
+  for (j = 0; j < locations.length; j++) {
+    marker_list = new google.maps.Marker({
+      position: new google.maps.LatLng(locations_list[j][1], locations_list[j][2]),
+      map: map_list
     });
 
-    var infowindow = new google.maps.InfoWindow();
-
-    var marker_list;
-
-    for (j = 0; j < locations.length; j++) {
-      marker_list = new google.maps.Marker({
-        position: new google.maps.LatLng(locations_list[j][1], locations_list[j][2]),
-        map: map_list
-      });
-
-      google.maps.event.addListener(marker_list, 'click', (function(marker_list, j) {
-        return function() {
-          infowindow.setContent(locations_list[j][0]);
-          infowindow.open(map_list, marker_list);
-        }
-      })(marker_list, j));
-    }
-
+    google.maps.event.addListener(marker_list, 'click', (function(marker_list, j) {
+      return function() {
+        infowindow.setContent(locations_list[j][0]);
+        infowindow.open(map_list, marker_list);
+      }
+    })(marker_list, j));
   }
+
   /*- /map for listings page only -*/
 
 
   /*- map for property page only -*/
-  function propMap() {
 
-    //****- inputs should be placed here eg. -> [ 'location txt', 'lat', 'lng', 'id#' ]
-    var locations_prop = ['Park Merced Office', 37.7183, 122.4810, 1];
+  //****- inputs should be placed here eg. -> [ 'location txt', 'lat', 'lng', 'id#' ]
+  var locations_prop = ['Park Merced Office', 37.7183, 122.4810, 1];
 
-    //-- center at coordinates
-    var map_prop = new google.maps.Map(document.getElementById('gmap'), {
-      zoom: 10,
-      center: new google.maps.LatLng(locations_prop[1], locations_prop[2]),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+  //-- center at coordinates
+  var map_prop = new google.maps.Map(document.getElementById('gmap'), {
+    zoom: 10,
+    center: new google.maps.LatLng(locations_prop[1], locations_prop[2]),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
 
-    var infowindow_prop = new google.maps.InfoWindow();
+  var infowindow_prop = new google.maps.InfoWindow();
 
-    var marker_prop = new google.maps.Marker({
-      position: new google.maps.LatLng(locations_prop[1], locations_prop[2]),
-      map: map_prop
-    });
+  var marker_prop = new google.maps.Marker({
+    position: new google.maps.LatLng(locations_prop[1], locations_prop[2]),
+    map: map_prop
+  });
 
-      google.maps.event.addListener(marker_prop, 'click', (function(marker_prop) {
-        return function() {
-          infowindow.setContent(locations_prop[0]);
-          infowindow.open(map_prop, marker_prop);
-        }
-      })(marker_prop));
+  google.maps.event.addListener(marker_prop, 'click', (function(marker_prop) {
+    return function() {
+      infowindow.setContent(locations_prop[0]);
+      infowindow.open(map_prop, marker_prop);
     }
+  })(marker_prop));
+}
 
-  }
-  /*- /map for property page only -*/
+
+/*- /map for property page only -*/
 
 
 
