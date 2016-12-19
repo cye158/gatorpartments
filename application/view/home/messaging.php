88cfbde4d1800a93d69dd02f3575654a6930141d
mbain <mbain@mail.sfsu.edu>
2016-12-18 17:59:03 -0800

    Removing legacy inbox code.

--- application/view/home/messaging.php
+++ application/view/home/messaging.php
@@ -18,7 +18,7 @@ else{
       <?php
       $index = 0;
       foreach($messages as $message){ ?>
-        <a href="#" onclick="setContentBox( <?php echo $message->content; ?>)" class="list-group-item list-group-item-action">
+        <a onclick="setContentBox( <?php echo $message->content; ?>)" class="list-group-item list-group-item-action">
           <?php echo($listings[$index]->title); ?>
         </a>
         <?php } ?>
@@ -30,63 +30,27 @@ else{
         <?php
         $index = 0;
         foreach($messages as $message){ ?>
-        <a href="#" onclick="setContentBox(<?php echo $message->content; ?>)" class="list-group-item list-group-item-action">
-          <?php echo $messageUserNames[$index];
-          $index++; ?>
-        </a>
-        <?php } ?>
+          <a onclick="setContentBox(<?php echo $message->content; ?>)" class="list-group-item list-group-item-action">
+            <?php echo $messageUserNames[$index];
+            $index++; ?>
+          </a>
+          <?php } ?>
 
-      </div>
-      <div class="col-md-4 panel">Message content column
-        <br />
-        <div class="panel-body" id="messageContentBox">
-          <?php
-          echo('Message content goes here.');
-          ?>
+        </div>
+        <div class="col-md-4 panel">Message content column
+          <br />
+          <div class="panel-body" id="messageContentBox">
+            <?php
+            echo('Message content goes here.');
+            ?>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  <br>
+    <br>
 
-  <script type="text/javascript">
-  function setContentBox(messageText){
-    document.getElementById("messageContentBox").innerHTML = messageText;
-  }
-  </script>
-
-  <table id="table">
-    <tr>
-      <th id="property">Property</th>
-      <th id="messsageFrom">Message from:</th>
-      <th id="selectMessage">Click to select message</th>
-    </tr>
-    <?php $index = 0; ?>
-    <?php foreach ($messages as $message) { ?>
-
-      <tr>
-        <td headers="property"><a onclick="displayMessage(2);"><?php echo $listings[$index]->title; ?></a></td>
-        <td headers="messageFrom" id="message0002"><a onclick="displayMessage(2);"><?php echo $messageUserNames[$index]; $index++; ?></a></td>
-        <td headers="selectMessage" id="message0001"><a href="<?php echo URL . 'messages/showMessageDetail/' . $message->id ;?>"  class="btn btn-primary">Display Message</a></td>
-      </tr>
-      <?php   } ?>
-
-    </table>
-    <table>
-      <tr>
-        <td id="messageContent">
-        </td>
-      </tr>
-    </table>
-
-    <script>
-    function displayMessage(messageID){
-      if(messageID==1){
-        document.getElementById("messageContent").innerHTML = "Content of message 1";
-      }
-      elseif(messageID==2){
-        document.getElementById("messageContent").innerHTML = "Conent of message 2";
-      }
-
-    };
+    <script type="text/javascript">
+    function setContentBox(messageText){
+      document.getElementById("messageContentBox").innerHTML = messageText;
+    }
     </script>
