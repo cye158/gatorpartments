5c85495409ed24d2a08b23f9379e8dfb89e8702d
Xuan Zhang <Xuan Zhang>
2016-12-19 02:48:14 +0000

    add a button to contact back

--- application/view/home/messageDetail.php
+++ application/view/home/messageDetail.php
@@ -6,5 +6,9 @@
          <h3>Listing Id: <?php echo "	" . $message->listingId; ?></h3>
          <h3>Content</h3>
          <p><?php echo $message->content; ?></p>
+
+<form action="<?php echo URL . 'messages/contactBack/' . $messageUserName .'/'. $message->listingId ;?>" method = "POST" id="form1">
+ </form> 
+<button class="b b0 navbar-right msg-btn" style="background-color: #609b62;" type="submit" form="form1" name="submit_contact_back"><h4>Contact Back</h4></button>
     
 </div>
