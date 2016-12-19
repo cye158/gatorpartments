19ed8d96438e52765ab7137e97328742d8637bc0
mbain <mbain@mail.sfsu.edu>
2016-12-18 16:12:03 -0800

    Removed unnecessary script tag.

--- application/view/home/messaging.php
+++ application/view/home/messaging.php
@@ -14,9 +14,6 @@ else{
 <div class="panel">
   <div class="panel-body">
     <div class="col-md-3 list-group">Property column<br>
-      <script type="text/javascript">
-        function
-      </script>
       <?php
       $index = 0;
       foreach($messages as $message){
@@ -48,39 +45,39 @@ else{
     </div>
   </div>
   <br>
-<!--
+  <!--
   <table id="table">
-    <tr>
-      <th id="property">Property</th>
-      <th id="messsageFrom">Message from:</th>
-      <th id="selectMessage">Click to select message</th>
-    </tr>-->
-    <?php //$index = 0; ?>
-    <?php //foreach ($messages as $message) { ?>
-<!--
-      <tr>
-        <td headers="property"><a onclick="displayMessage(2);"><?php //echo $listings[$index]->title; ?></a></td>
-        <td headers="messageFrom" id="message0002"><a onclick="displayMessage(2);"><?php //echo $messageUserNames[$index]; $index++; ?></a></td>
-        <td headers="selectMessage" id="message0001"><a href="<?php //echo URL . 'messages/showMessageDetail/' . $message->id ;?>"  class="btn btn-primary">Display Message</a></td>
-      </tr>
-      <?php //  } ?>
+  <tr>
+  <th id="property">Property</th>
+  <th id="messsageFrom">Message from:</th>
+  <th id="selectMessage">Click to select message</th>
+</tr>-->
+<?php //$index = 0; ?>
+<?php //foreach ($messages as $message) { ?>
+  <!--
+  <tr>
+  <td headers="property"><a onclick="displayMessage(2);"><?php //echo $listings[$index]->title; ?></a></td>
+  <td headers="messageFrom" id="message0002"><a onclick="displayMessage(2);"><?php //echo $messageUserNames[$index]; $index++; ?></a></td>
+  <td headers="selectMessage" id="message0001"><a href="<?php //echo URL . 'messages/showMessageDetail/' . $message->id ;?>"  class="btn btn-primary">Display Message</a></td>
+</tr>
+<?php //  } ?>
 
-    </table>
-    <table>
-      <tr>
-        <td id="messageContent">
-        </td>
-      </tr>
-    </table>
+</table>
+<table>
+<tr>
+<td id="messageContent">
+</td>
+</tr>
+</table>
 
-    <script>
-    function displayMessage(messageID){
-      if(messageID==1){
-        document.getElementById("messageContent").innerHTML = "Content of message 1";
-      }
-      elseif(messageID==2){
-        document.getElementById("messageContent").innerHTML = "Conent of message 2";
-      }
+<script>
+function displayMessage(messageID){
+if(messageID==1){
+document.getElementById("messageContent").innerHTML = "Content of message 1";
+}
+elseif(messageID==2){
+document.getElementById("messageContent").innerHTML = "Conent of message 2";
+}
 
-    };
-  </script> -->
+};
+</script> -->
