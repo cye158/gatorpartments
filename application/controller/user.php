97640659d4d0cfaf236dfc6eeeb69ac15ec2dfa9
Miles Oliver Bainbridge <mbain@mail.sfsu.edu>
2016-12-19 03:31:18 +0000

    Update user.php

--- application/controller/user.php
+++ application/controller/user.php
@@ -27,7 +27,7 @@ class User extends Controller {
 
     require APP . "view/_templates/header.php";
     require APP . "view/user/register.php";
-    //require APP . "view/_templates/footer.php";
+    require APP . "view/_templates/footer.php";
   }
 
   public function login(){
